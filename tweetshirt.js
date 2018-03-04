9add08eafced31ca569e8ae5eabb3c54af119f1e
Ian <ian (at) ianrobinson.net>
2018-03-03 20:22:30 -0500

    Add button and ensure tweetshirt.js is connected

---
+++ tweetshirt.js
@@ -0,0 +1,7 @@
+// Time to get computational, with JavaScript
+
+window.onload = function() {
+  console.log("Preview Button Clicked");
+  var button = document.getElementById("previewButtton");
+  button.onclick = previewHandler;
+}
