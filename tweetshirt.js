a43bc588666a853825ce5c4ecd99698304b8e1ea
Ian <ian (at) ianrobinson.net>
2018-03-05 14:39:17 -0500

    Add circle functionality with DRY solutions which could be implemented. Also expanded the size of the shapes

--- tweetshirt.js
+++ tweetshirt.js
@@ -19,12 +19,16 @@ function previewHandler() {
       // Passing the canvas and the context to drawSquares to execute drawing function
       drawSquare(canvas, context);
     }
+  } else if (shape == "circles") {
+    for (var circles = 0; circles < 20; circles += 1) {
+      drawCircle(canvas, context);
+    }
   }
 }
 
 function drawSquare(canvas, context) {
   // We choose 40 px so the squares don't get too long
-  var w = Math.floor(Math.random() * 40);
+  var w = Math.floor(Math.random() * 60);
   // We multiply random numbers by height and width to get appropriate sized squares
   var x = Math.floor(Math.random() * canvas.width);
   var y = Math.floor(Math.random() * canvas.height);
@@ -49,14 +53,25 @@ function drawSquare(canvas, context) {
   // context.closePath()
   // context.fillStyle = 'red';
   // context.fill();
-  // function degreesToRadians(degrees) {
-  //   return (degrees * Math.PI)/180;
-  // }
   // // END CIRCLE PRACTICE ///
   var colors = ["red", "blue", "green", "yellow"];
   
   context.fillStyle = colors[Math.floor(Math.random() * colors.length)];
   context.fillRect(x, y, w, w);
+}
+
+function drawCircle(canvas, context) {
+  var radius = Math.floor(Math.random() * 60);
+  var x = Math.floor(Math.random() * canvas.width);
+  var y = Math.floor(Math.random() * canvas.height);
+  
+  context.beginPath();
+  context.arc(x, y, radius, 0, degreesToRadians(360), true);
+  context.closePath();
+  
+  var colors = ["red", "blue", "green", "yellow"];  
+  context.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+  context.fill();
 }
 
 function fillBackgroundColor(canvas, context) {
@@ -65,4 +80,9 @@ function fillBackgroundColor(canvas, context) {
   var bgColor = selectObj.options[index].value;
   context.fillStyle = bgColor;
   context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// Use this function to support with drawing circles in drawX() functions
+function degreesToRadians(degrees) {
+  return (degrees * Math.PI)/180;
 }
